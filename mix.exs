b3259c57557480b0457237111a14eff1aa0ea56b
Mateusz Front <mateusz.front@swmansion.com>
2024-09-11 12:10:09 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule Boombox.Mixfile do
       {:membrane_core, "~> 1.1"},
       {:membrane_webrtc_plugin,
        github: "membraneframework/membrane_webrtc_plugin", branch: "ex-webrtc-0.4"},
+      {:ex_sdp, "~> 1.0", override: true},
       {:membrane_opus_plugin, "~> 0.20.3"},
       {:membrane_aac_plugin, "~> 0.18.0"},
       {:membrane_aac_fdk_plugin, "~> 0.18.0"},
