6bd8d5348a0f32490f74dcfa8a25b4b395eb9fba
noarkhh <jakub.pryc@gmail.com>
2025-04-02 14:25:01 +0200

    Add realtimers before hls sink

--- lib/boombox/internal_bin/hls.ex
+++ lib/boombox/internal_bin/hls.ex
@@ -50,6 +50,7 @@ defmodule Boombox.InternalBin.HLS do
               output_stream_format: Membrane.AAC,
               transcoding_policy: transcoding_policy
             })
+            |> child(:hls_audio_realtimer, Membrane.Realtimer)
             |> via_in(Pad.ref(:input, :audio),
               options: [encoding: :AAC, segment_duration: Time.milliseconds(2000)]
             )
@@ -61,6 +62,7 @@ defmodule Boombox.InternalBin.HLS do
               output_stream_format: %H264{alignment: :au, stream_structure: :avc3},
               transcoding_policy: transcoding_policy
             })
+            |> child(:hls_video_realtimer, Membrane.Realtimer)
             |> via_in(Pad.ref(:input, :video),
               options: [encoding: :H264, segment_duration: Time.milliseconds(2000)]
             )
