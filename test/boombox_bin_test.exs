d3ff5f878b6ea1fdee5e5073a9b006482e615495
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-04-14 14:49:37 +0200

    Fix test

--- test/boombox_bin_test.exs
+++ test/boombox_bin_test.exs
@@ -12,6 +12,7 @@ defmodule Boombox.BinTest do
     Opus,
     RawAudio,
     RawVideo,
+    RemoteStream,
     Testing,
     Transcoder,
     VP8
@@ -21,15 +22,15 @@ defmodule Boombox.BinTest do
 
   @bbb_mp4 "test/fixtures/bun10s.mp4"
 
-  @video_formats Macro.escape([
-                   {H264, :avc3, :au},
-                   {H264, :annexb, :nalu},
-                   RawVideo,
-                   VP8,
-                   nil
-                 ])
+  @video_formats_for_sink_bin Macro.escape([
+                                {H264, :avc3, :au},
+                                {H264, :annexb, :nalu},
+                                RawVideo,
+                                VP8,
+                                nil
+                              ])
 
-  @audio_formats Macro.escape([AAC, Opus, RawAudio, nil])
+  @audio_formats_for_sink_bin Macro.escape([AAC, Opus, RawAudio, nil])
 
   defmodule Format do
     @spec to_string(any()) :: String.t()
@@ -37,22 +38,22 @@ defmodule Boombox.BinTest do
     def to_string(format), do: inspect(format)
   end
 
-  for video_format <- @video_formats, audio_format <- @audio_formats do
+  for video_format <- @video_formats_for_sink_bin, audio_format <- @audio_formats_for_sink_bin do
     if video_format != nil or audio_format != nil do
       @tag :tmp_dir
-      test "Boombox bin with input pad when video is #{Format.to_string(video_format)} and audio is #{Format.to_string(audio_format)}",
+      test "Boombox.Bin with input pad when video is #{Format.to_string(video_format)} and audio is #{Format.to_string(audio_format)}",
            %{tmp_dir: tmp_dir} do
         video_format =
           with {H264, stream_structure, alignment} <- unquote(video_format) do
             %H264{stream_structure: stream_structure, alignment: alignment}
           end
 
-        do_test(video_format, unquote(audio_format), tmp_dir)
+        do_test_sink_bin(video_format, unquote(audio_format), tmp_dir)
       end
     end
   end
 
-  defp do_test(video_format, audio_format, tmp_dir) do
+  defp do_test_sink_bin(video_format, audio_format, tmp_dir) do
     out_file = Path.join(tmp_dir, "out.mp4")
 
     spec = [
@@ -83,6 +84,50 @@ defmodule Boombox.BinTest do
     end
   end
 
+  @video_formats_for_source_bin Macro.escape([H264, VP8, RawVideo])
+  @audio_formats_for_source_bin Macro.escape([AAC, Opus, RawAudio])
+
+  for video_codec <- @video_formats_for_source_bin,
+      audio_codec <- @audio_formats_for_source_bin do
+    test "Boombox.Bin with source pad when video is #{Format.to_string(video_codec)} and audio is #{Format.to_string(audio_codec)}" do
+      do_test_source_bin(
+        unquote(video_codec),
+        unquote(audio_codec)
+      )
+    end
+  end
+
+  defp do_test_source_bin(video_codec, audio_codec) do
+    spec = [
+      child(:boombox, %Boombox.Bin{input: @bbb_mp4})
+      |> via_out(:output,
+        options: [
+          kind: :audio,
+          codec: audio_codec
+        ]
+      )
+      |> child(:audio_sink, Testing.Sink),
+      get_child(:boombox)
+      |> via_out(:output,
+        options: [
+          kind: :video,
+          codec: video_codec
+        ]
+      )
+      |> child(:video_sink, Testing.Sink)
+    ]
+
+    pipeline = Testing.Pipeline.start_link_supervised!(spec: spec)
+
+    assert_sink_stream_format(pipeline, :audio_sink, audio_format)
+    assert audio_format.__struct__ == audio_codec
+
+    assert_sink_stream_format(pipeline, :video_sink, video_format)
+    assert video_format.__struct__ == video_codec
+
+    Testing.Pipeline.terminate(pipeline)
+  end
+
   defp audio_fixture(Opus), do: "test/fixtures/ref_bun10s_opus_aac.mp4"
   defp audio_fixture(_format), do: "test/fixtures/ref_bun10s_aac.mp4"
 
@@ -104,54 +149,54 @@ defmodule Boombox.BinTest do
     ]
   end
 
-  test "source bin" do
-    spec = [
-      child(:boombox_source, %Boombox.Bin{input: "test/fixtures/bun10s.mp4"})
-      |> via_out(:output, options: [kind: :video])
-      |> via_in(:input, options: [kind: :video])
-      |> child(:boombox_sink, %Boombox.Bin{
-        output: "xd.mp4"
-      }),
-      get_child(:boombox_source)
-      |> via_out(:output, options: [kind: :audio])
-      |> via_in(:input, options: [kind: :audio])
-      |> get_child(:boombox_sink)
-    ]
-
-    pipeline = Testing.Pipeline.start_link_supervised!(spec: spec)
-    assert_pipeline_notified(pipeline, :boombox_sink, :processing_finished)
-    Testing.Pipeline.terminate(pipeline)
-    System.cmd("rm", ["xd.mp4"])
-  end
-
   describe "Boombox.Bin raises when" do
     test "it has input pad linked and `:input` option set at the same time" do
       spec =
-        child(Testing.Source)
+        child(%Testing.Source{
+          stream_format: %RemoteStream{content_format: Opus, type: :packetized}
+        })
         |> via_in(:input, options: [kind: :audio])
         |> child(%Boombox.Bin{
           input: {:webrtc, "ws://localhost:5432"},
           output: {:webrtc, "ws://localhost:5433"}
         })
 
-      do_test_raise(spec)
+      assert {:error, {%Membrane.ParentError{}, _stacktrace}} =
+               Testing.Pipeline.start(spec: spec)
     end
 
     test "its input and output pads are linked at the same time" do
       spec =
-        child(Testing.Source)
+        child(%Testing.Source{
+          stream_format: %RemoteStream{content_format: Opus, type: :packetized}
+        })
         |> via_in(:input, options: [kind: :audio])
         |> child(Boombox.Bin)
         |> via_out(:output, options: [kind: :audio])
         |> child(Testing.Sink)
 
-      do_test_raise(spec)
+      assert {:error, {%Membrane.ParentError{}, _stacktrace}} =
+               Testing.Pipeline.start(spec: spec)
     end
 
     @tag :tmp_dir
     test "pad is linked after `handle_playing/2`", %{tmp_dir: tmp_dir} do
+      generator =
+        fn _state, _demand ->
+          receive do
+            :will_not_match -> :ok
+          after
+            100 -> :ok
+          end
+
+          {[redemand: :output], nil}
+        end
+
       spec =
-        child(Testing.Source)
+        child(%Testing.Source{
+          stream_format: %RemoteStream{content_format: Opus, type: :packetized},
+          output: {nil, generator}
+        })
         |> via_in(:input, options: [kind: :audio])
         |> child(:boombox, %Boombox.Bin{
           output: Path.join(tmp_dir, "file.mp4")
@@ -164,18 +209,15 @@ defmodule Boombox.BinTest do
       assert Process.alive?(supervisor)
 
       new_spec =
-        child(Testing.Source)
+        child(%Testing.Source{
+          stream_format: %RemoteStream{content_format: VP8, type: :packetized},
+          output: {nil, generator}
+        })
         |> via_in(:input, options: [kind: :video])
         |> get_child(:boombox)
 
       Testing.Pipeline.execute_actions(pipeline, spec: new_spec)
       assert_receive {:DOWN, ^ref, :process, _supervisor, _reason}
     end
-  end
-
-  defp do_test_raise(spec) do
-    {:ok, supervisor, _pipeline} = Testing.Pipeline.start(spec: spec)
-    ref = Process.monitor(supervisor)
-    assert_receive {:DOWN, ^ref, :process, _supervisor, _reason}
   end
 end
