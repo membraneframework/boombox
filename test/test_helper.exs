87d5a6eb08882915a1e88488f38a217457775704
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-05-26 14:24:55 +0200

    Set max cases

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,6 @@
-ExUnit.start(capture_log: true)
+max_cases =
+  if System.get_env("CIRCLECI") == "true",
+    do: 1,
+    else: System.schedulers_online() * 2
+
+ExUnit.start(capture_log: true, max_cases: max_cases)
