d79dcaf908798c9061f04c01471a07d1f37b1af3
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-05-26 14:17:09 +0200

    Revert "Fix typo"
    This reverts commit ead5a11ba7aa4948554eaa0fb6f79ff73c06d9ea.

--- test/browser_test.exs
+++ test/browser_test.exs
@@ -1,5 +1,5 @@
 defmodule Boombox.BrowserTest do
-  use ExUnit.Case, async: System.get_env("CIRCLECI") != "true"
+  use ExUnit.Case, async: System.get_env("CIRCLECI") == "true"
 
   # Tests from this module are currently switched off on the CI because
   # they raise some errors there, that doesn't occur locally (probably
