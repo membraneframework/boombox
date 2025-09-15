71558c5b9ba39de30a8533961f46271829b7db52
noarkhh <jakub.pryc@gmail.com>
2025-09-15 10:45:54 +0200

    Remove leftover prints

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -6,9 +6,6 @@
 import sys
 import os
 
-# sys.path.insert(0, os.path.abspath("../src/"))
-print(os.path.abspath(__file__))
-print(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
 )
