749160391fba9c4ef4da5dd40f5e8383f4742fe1
Łukasz Kita <lukasz.kita0@gmail.com>
2026-03-04 16:35:54 +0100

    Turn spawn_link into Task.start_link

--- lib/boombox.ex
+++ lib/boombox.ex
@@ -369,15 +369,12 @@ defmodule Boombox do
 
     case opts do
       %{input: {:stream, _stream_opts}} ->
-        consumer =
-          spawn_link(fn ->
-            procs = Pipeline.start_link(opts)
-            source = await_source_ready()
-            Process.monitor(procs.supervisor)
-            consume_stream(stream, source, procs)
-          end)
-
-        {:ok, consumer}
+        Task.start_link(fn ->
+          procs = Pipeline.start_link(opts)
+          source = await_source_ready()
+          Process.monitor(procs.supervisor)
+          consume_stream(stream, source, procs)
+        end)
 
       opts ->
         procs = Pipeline.start_link(opts)
